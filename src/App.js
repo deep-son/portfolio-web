ef9030a1c66e1defa295d12de5c7ec643fe2c267
Deepanshu Sonparote <deepanshusonparote@gmail.com>
2023-06-10 00:07:54 +0200

    update: updated package.json

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
